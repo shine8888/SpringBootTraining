b7c01cf39fce036f50140aa187b02ba62b614472
shine8888 <kieuvanquang@luvina.net>
2020-09-10 16:47:55 +0700

    mtfk

---
+++ src/main/java/com/main/models/Vehicle.java
@@ -0,0 +1,68 @@
+package com.main.models;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Entity
+@Data
+@AllArgsConstructor
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class,
+property = "id")
+public class Vehicle {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	private String name;
+	@ManyToOne
+	@JoinColumn(name = "vehicletypeid", insertable = false,  updatable = false)
+	private VehicleType vehicleType;
+	private Integer vehicletypeid;
+	
+	private String vehicleNumber;
+	
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date acquisitionDate;
+	private String description;
+	
+	@ManyToOne
+	@JoinColumn(name="vehiclemakeid", insertable = false, updatable = false)
+	private VehicleStatus vehicleStatus;
+	private Integer vehiclestatus;
+	
+	private String netWeight;
+	
+	@ManyToOne
+	@JoinColumn(name = "employeeid", insertable = false, updatable = false)
+	private Employee inCharge;
+	private Integer employeeid;
+	
+	@ManyToOne
+	@JoinColumn(name = "vehiclemodelid", insertable = false, updatable = false)
+	private VehicleModel vehicleModel;
+	private Integer vehiclemodelid;
+	
+	@ManyToOne
+	@JoinColumn(name = "locationid", insertable = false, updatable = false)
+	private Location currentLocation;
+	private Integer locationid;
+	
+	private String remarks;
+}
---
+++ src/main/java/com/main/services/InvoiceService.java
@@ -0,0 +1,8 @@
+package com.main.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class InvoiceService {
+
+}
